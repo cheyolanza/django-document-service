52474a82e03cfe60d4a2cfef8c76eb90cfd21685
Jose Lanza <ina_jlanza@contractor.indeed.com>
2025-06-01 23:35:31 -0400

    ARREGLADO AHORA FUNCIONA CON DOCKER!

--- docservice/docservice/settings.py
+++ docservice/docservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,11 @@ WSGI_APPLICATION = 'docservice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'USER': 'postgres',
-        'PASSWORD' : 'password',
-        'HOST':'0.0.0.0',
-        'PORT':'5432'
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD' : os.environ.get('DB_PASSWORD'),
+        'HOST':os.environ.get('DB_HOST'),
+        'PORT':os.environ.get('DB_PORT')
     }
 }
 
